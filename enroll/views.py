1e87dcb47175c2926339c2308e0d0028b2d0c97b
Hiisagar <sagar6263716@gmail.com>
2026-05-02 18:53:36 +0530

    make read and shgw data functionality

--- enroll/views.py
+++ enroll/views.py
@@ -1,7 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .models import Student
 
 # Create your views here.
 
 
 def add_show(request):
-    return render(request,'enroll/readandshow.html')
+    if request.method == 'POST':
+        name = request.POST.get('name',"")
+        age = request.POST.get('age',"")
+        phone = request.POST.get('phone',"")
+
+        student = Student(name=name,age=age,phone=phone)
+        student.save()
+        return redirect('/')
+    
+    student_data = Student.objects.all()
+
+    return render(request,'enroll/readandshow.html',{'data': student_data})

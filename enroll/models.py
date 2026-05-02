58cb7a205320465f907c2c5bad2c312fc371eb2f
Hiisagar <sagar6263716@gmail.com>
2026-05-02 18:07:33 +0530

    make superadmin and create student information model

--- enroll/models.py
+++ enroll/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Student(models.Model):
+    std_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    age = models.IntegerField()
+    phone = models.CharField(max_length=15)
+
+    def __str__(self):
+        return self.name
